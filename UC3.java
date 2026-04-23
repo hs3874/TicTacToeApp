cda4a0f3a3586ab70d7ce5a0c3aedaa4da6e9985
Shivansh Singh <hs3874@srmist.edu.in>
2026-04-23 20:43:53 +0530

    User Case-3 Completed

---
+++ UC3.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class UC3 {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    static char[][] board = new char[3][3];
+    static Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
+
+        
+        int userChoice = getUserSlot();
+        System.out.println("You selected slot: " + userChoice);
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and will play first.");
+        } else {
+            System.out.println("Computer won the toss and will play first.");
+        }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+
+    
+    static int getUserSlot() {
+        int slot;
+
+        while (true) {
+            System.out.print("Enter a slot number (1-9): ");
+            slot = sc.nextInt();
+
+            if (slot >= 1 && slot <= 9) {
+                return slot;
+            } else {
+                System.out.println("Invalid input! Please enter between 1 and 9.");
+            }
+        }
+    }
+}
